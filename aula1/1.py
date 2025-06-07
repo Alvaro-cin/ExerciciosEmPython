1436f541f7fbe559678daf534051942de1100940
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-06-07 16:02:18 -0300

    Python

---
+++ aula1/1.py
@@ -0,0 +1,2 @@
+valor = int(input("Me informe um número: "))
+print("O antecessor de ", valor , " e ", valor-1)
---
+++ aula1/2.py
@@ -0,0 +1,3 @@
+b = float(input("Me informe a base do retângulo: "))
+a = float(input("Me informe a altura do retângulo: " ))
+print("A area do retângulo e: ", b*a)
