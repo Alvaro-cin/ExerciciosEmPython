3d54e4684a0b950a9a44c0f681939af44d6ed2df
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-06-20 10:51:35 -0300

    Update F.py

--- EstruturasCondicionais-Exercicios/F.py
+++ EstruturasCondicionais-Exercicios/F.py
@@ -1,1 +1,13 @@
- 
+print("Bem vindo a calculadora simples")
+n1 = float(input("Digite o Primeiro Valor: "))
+n2 = float(input("Digite o Segundo Valor: "))
+op = str(input("Que operacao quer realizar? ( + , - , / , * ) "))
+if op == '-':
+    print(n1 , " - ",n2 ,"e " , n1-n2)
+elif op == '+':
+     print(n1 , " + ",n2 ,"e " , n1+n2)
+elif op == '/':
+      print(n1 , " / ",n2 ,"e " , n1/n2)
+elif op == '*':
+      print(n1 , " * ",n2 ,"e " , n1*n2)
+      print
