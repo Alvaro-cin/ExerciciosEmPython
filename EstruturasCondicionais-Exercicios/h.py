7c870c7fc55ff4d3853c0c18596c968ca47d5069
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-04 10:36:08 -0300

    C++

---
+++ EstruturasCondicionais-Exercicios/h.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um número na faixa de 1 até 9: "))
+if n1>=1 and n1 <=9:
+    print("O valor está na faixa permitida")
+else:
+    print("O valor está fora da faixa permitida")
+   
