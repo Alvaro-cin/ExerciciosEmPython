e0b83ecaf7d7385e0890b66db17a6d80787515ec
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-04 10:38:02 -0300

    Python

--- EstruturasCondicionais-Exercicios/h.py
+++ EstruturasCondicionais-Exercicios/h.py
@@ -1,6 +1,6 @@
 n1 = int(input("Digite um número na faixa de 1 até 9: "))
 if n1>=1 and n1 <=9:
-    print("O valor está na faixa permitida")
+    print("O valor está na faixa permitida!")
 else:
-    print("O valor está fora da faixa permitida")
+    print("O valor está fora da faixa permitida!")
    
