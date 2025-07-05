c67661b4cf63b608aafb455b1744b8e408eed89c
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-05 10:59:14 -0300

    Python

---
+++ Estruturas-de-Repetição-Exercicios/H.py
@@ -0,0 +1,9 @@
+print("Sequência de fibonacci:")
+anterior = 0
+depois = 1
+for a in range(0,16):
+    proximo = anterior + depois
+    print(proximo)
+    anterior = depois
+    depois = proximo
+    
