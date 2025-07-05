7c9c2506dc9e38d7153cf821f8c1fc8161b66f87
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-05 10:01:51 -0300

    Python

--- Estruturas-de-Repetição-Exercicios/A.py
+++ Estruturas-de-Repetição-Exercicios/A.py
@@ -1,1 +1,4 @@
-hjjjjjjjjjjjhj
+print("Aqui estão os numeros de 15 a 200:")
+for a in range (15,201):
+ print(a)
+  
---
+++ Estruturas-de-Repetição-Exercicios/B.py
@@ -0,0 +1,4 @@
+n = int(input("Digite o número:"))
+print("Aqui esta sua tabuada:")
+for a in range(0,11):
+ print(n, " X ",a," = ",n*a)
